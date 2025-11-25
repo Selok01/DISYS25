72ab91d6d4d5c54d698bc3981452ad5f4addba65
Sebastian Blok <segb@itu.dk>
2025-11-25 15:19:22 +0100

    first fully working iteration of auction system + Makefile for easy testing

--- auc-replication/clients/main.go
+++ auc-replication/clients/main.go
@@ -14,6 +14,28 @@ import (
 	pb "auc-replication/grpc/pb"
 )
 
+func Bid(amount int32, id int32) *pb.ClientRequest {
+
+	return &pb.ClientRequest{
+		Payload: &pb.ClientRequest_Bid{
+			Bid: &pb.BidRequest{
+				ClientId: id,
+				Amount:   amount,
+			},
+		},
+	}
+}
+
+func Result(id int32) *pb.ClientRequest {
+	return &pb.ClientRequest{
+		Payload: &pb.ClientRequest_Result{
+			Result: &pb.ResultRequest{
+				ClientId: id,
+			},
+		},
+	}
+}
+
 func main() {
 	conn, err := grpc.NewClient("localhost:8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
@@ -25,31 +47,21 @@ func main() {
 	client := pb.NewAuctionServiceClient(conn)
 	clientID := int32(time.Now().Unix() % 1000)
 
+	for range 30 {
+		// ----- Send Bid -----
+		bid := Bid(int32(random.Intn(10000)), clientID)
+		reply, _ := client.Auction(context.Background(), bid)
+		log.Print(reply)
 
-	// ----- Send Bid -----
-	bid := &pb.ClientRequest{
-		Payload: &pb.ClientRequest_Bid{
-			Bid: &pb.BidRequest{
-				ClientId: clientID,
-				Amount: int32(random.Intn(100)),
-			},
-		},
+		time.Sleep(1 * time.Second)
+
+		// ----- Get current state -----
+		result := Result(clientID)
+
+		reply, _ = client.Auction(context.Background(), result)
+		log.Print(reply)
+
+		time.Sleep(1 * time.Second)
 	}
 
-	_, error := client.Auction(context.Background(), bid)
-	log.Print(error)
-
-	// ----- Get current state -----
-	result := &pb.ClientRequest{
-		Payload: &pb.ClientRequest_Result{
-			Result: &pb.ResultRequest{
-				ClientId: clientID,
-			},
-		},
-	}
-
-	_, error = client.Auction(context.Background(), result)
-	log.Print(error)
-
-	select {}
 }
--- auc-replication/servers/main.go
+++ auc-replication/servers/main.go
@@ -9,6 +9,7 @@ import (
 	"os/signal"
 	"sync"
 	"syscall"
+	"time"
 
 	"google.golang.org/grpc"
 
@@ -17,14 +18,14 @@ import (
 
 type auctionService struct {
 	pb.UnimplementedAuctionServiceServer
-	mu      sync.Mutex
-	bidders map[int32]int32
-	highestBid, highestBidder int32
-	over bool
+	mu                               sync.Mutex
+	bidders                          map[int32]int32
+	highestBid, highestBidder, round int32
+	over                             bool
 }
 
 func NewServer() *auctionService {
-	return &auctionService {
+	return &auctionService{
 		bidders: make(map[int32]int32),
 	}
 }
@@ -33,11 +34,11 @@ func (a *auctionService) Auction(ctx context.Context, req *pb.ClientRequest) (*p
 	var res string
 
 	switch payload := req.Payload.(type) {
-		case *pb.ClientRequest_Bid:
-			res = a.Bid(payload.Bid)
+	case *pb.ClientRequest_Bid:
+		res = a.Bid(payload.Bid)
 
-		case *pb.ClientRequest_Result:
-			res = a.Result()
+	case *pb.ClientRequest_Result:
+		res = a.Result()
 	}
 
 	return &pb.ServerReply{Ack: res}, nil
@@ -47,36 +48,49 @@ func (a *auctionService) Bid(req *pb.BidRequest) string {
 	currentBid := req.Amount
 	p := req.ClientId
 
-	if a.highestBid < currentBid {
-		a.highestBid = currentBid
-		a.highestBidder = p
+	if a.over {
+		return "fail: auction is over"
 	}
 
-	if _, exists := a.bidders[p]; !exists {
-		a.bidders[p] = currentBid
-	} 
-
-	if a.bidders[p] < currentBid {
-		a.bidders[p] = currentBid
-		return "success"
+	if currentBid <= a.highestBid {
+		return "fail"
 	}
 
-	return "fail"
+	// Update highest bid + highest bidder
+	a.highestBid = currentBid
+	a.highestBidder = p
+
+	// Record bidder's best bid
+	a.bidders[p] = currentBid
+
+	return "success"
 }
 
 func (a *auctionService) Result() string {
-	var result string 
+	var result string
 
 	if a.over {
 		result = fmt.Sprintf("Participant %d won the auction with a bet of %d", a.highestBidder, a.highestBid)
 		return result
 	}
 
-
 	result = fmt.Sprintf("Auction is not over yet, current highest bid is: %d", a.highestBid)
-	return  result
+	return result
 }
 
+func (a *auctionService) reset() {
+
+	a.bidders = make(map[int32]int32)
+	a.highestBid = 0
+	a.highestBidder = 0
+	a.over = false
+	a.round++
+}
+
+func (a *auctionService) closeAuc() {
+	a.over = true
+	log.Printf("Auction %d ended. Winner: %d with %d", a.round, a.highestBidder, a.highestBid)
+}
 
 /*
 
@@ -99,19 +113,33 @@ func main() {
 		log.Fatalf("Failed to listen on port 8080: %v", err)
 	}
 
-	//broadcast2Clients(lis.Addr())
-
 	grpcServer := grpc.NewServer()
 	server := NewServer()
 	pb.RegisterAuctionServiceServer(grpcServer, server)
 
-	
 	log.Println("Server started, now listening on :8080")
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, os.Interrupt, syscall.SIGINT)
 
-	<-sigChan
-	grpcServer.GracefulStop()
-	lis.Close()
-	log.Println("Server closed")
+	go func() {
+		for {
+			server.reset()
+			time.Sleep(15 * time.Second)
+			server.closeAuc()
+		}
+	}()
+
+	go func() {
+
+		sigChan := make(chan os.Signal, 1)
+		signal.Notify(sigChan, os.Interrupt, syscall.SIGINT)
+
+		<-sigChan
+		grpcServer.GracefulStop()
+		lis.Close()
+		log.Println("Server closed")
+
+	}()
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
 }
